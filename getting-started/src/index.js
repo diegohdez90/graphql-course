2db87f57cbd7f5df66d54b4fc76cba3d7bc54fcd
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-05 18:56:20 -0600

    feat: add math operations

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -1,4 +1,9 @@
 import id, { greeting, location, message, myName } from "./module";
+import add, { substract } from './mathematics';
+
 
 console.log(message, myName, location, id);
 console.log(greeting())
+
+console.log(add(5,8));
+console.log(substract(10,7));
