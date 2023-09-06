a7de908e45e1b3b6ed8a4e9906d46d0208d6f098
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-06 12:44:24 -0600

    feat: add query definition

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -1,26 +1,53 @@
 import { createServer } from 'http';
 import { createYoga, createSchema } from 'graphql-yoga';
 
+const dummyQuery = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+const dummyResolvers = {
+  Query: {
+    hello: () => 'First Query',
+    name: () => 'My name is Diego',
+    location: () => 'Puebla, Pue.',
+    bio: () => `Frontend developer
+      with experience in JavaScript.`
+  }
+};
+
+const queryProduct = `
+  type Query {
+    id: ID
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`;
+
+const resolversProduct = {
+  Query: {
+    id: () => '1234567890',
+    title: () => 'Apple',
+    price: () => 1.99,
+    releaseYear: () => 1981,
+    rating: () => 4.9,
+    inStock: () => true
+  }
+}
+
+
 createServer(
   createYoga({
     schema: createSchema({
-      typeDefs: `
-        type Query {
-          hello: String!
-          name: String!
-          location: String!
-          bio: String!
-        }
-      `,
-      resolvers: {
-        Query: {
-          hello: () => 'First Query',
-          name: () => 'My name is Diego',
-          location: () => 'Puebla, Pue.',
-          bio: () => `Frontend developer
-            with experience in JavaScript.`
-        }
-      }
+      typeDefs: queryProduct,
+      resolvers: resolversProduct
     })
   })
 ).listen(4000, () => {
