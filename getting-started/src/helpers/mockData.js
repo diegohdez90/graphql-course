3bdb326706767b1b2a7e63f042ca6b044dc44d63
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-11 16:51:21 -0600

    feat: add mock data

---
+++ getting-started/src/helpers/mockData.js
@@ -0,0 +1,37 @@
+const users = [{
+  id: 1,
+  name: 'Diego',
+  email: 'diegohdez12@gmail.com'
+}, {
+  id: 2,
+  name: 'John',
+  email: 'johndoe@gmail.com'
+}, {
+  id: 3,
+  name: 'Juan',
+  email: 'juanperez@gmail.com'
+}];
+
+const posts = [{
+  id: 1,
+  title: 'Getting Stated with GraphQL',
+  body: 'This is a sample tutorial in how to develop application with...',
+  published: true
+}, {
+  id: 2,
+  title: 'React Hooks',
+  body: 'The new feature in coding React application and develop functional components in JS and Typescript...',
+  published: false
+}, {
+  id: 3,
+  title: 'Getting Stated with NodeJS',
+  body: 'Runtime environment to develop JS applications...',
+  published: true
+
+}]
+
+
+export {
+  users,
+  posts
+};
