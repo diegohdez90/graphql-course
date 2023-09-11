ac693c873149c7bd45370186b5fccff2292821b2
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-11 17:46:55 -0600

    feat: add more relationship

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -39,6 +39,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -101,6 +102,11 @@ const resolvers = {
     author(parent, args, ctx, info) {
       const authorId = parent.author;
       return users.find(user => user.id === authorId);
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => post.author === parent.id)
     }
   }
 }
