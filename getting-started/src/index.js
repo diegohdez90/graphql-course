24e9423d1ce11c5bc22b9d6aefd274c244469171
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-12 18:03:48 -0600

    feat: add retrieve commets by user

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -41,6 +41,7 @@ const typeDefs = `
     email: String!
     age: Int
     posts: [Post!]!
+    comments: [Comment]!
   }
 
   type Post {
@@ -122,6 +123,9 @@ const resolvers = {
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter(post => post.author === parent.id)
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => comment.user === parent.id)
     }
   },
   Comment: {
