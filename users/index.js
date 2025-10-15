0b3433499b684910eeddf02988c0b8fdaef1c7d7
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-10-15 14:46:18 -0600

    feat(app): setup schema query to graphql

--- users/index.js
+++ users/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const graphql = require('express-graphql').graphqlHTTP
+const queries = require('./queries')
 
 const app = express()
 
 app.use('/graphql', graphql({
+    schema: queries,
     graphiql: true
 }))
 
