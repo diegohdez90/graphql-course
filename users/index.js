5edd092ba28e288bd73d462da45fa07a49847eb3
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-10-15 13:30:17 -0600

    feat(app): init graphql app config

--- users/index.js
+++ users/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const graphql = require('express-graphql').graphqlHTTP
 
 const app = express()
+
+app.use('/graphql', graphql({
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log('Listening in port 4000')
