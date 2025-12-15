2cdc60dfb77161f007a524b95b8ed802e74c7525
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-12-15 10:50:32 -0600

    feat(queries): add company query model

--- users/queries/index.js
+++ users/queries/index.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql');
 const axios = require('axios')
 const User = require('../schema/user');
+const Company = require('../schema/company')
 const _ = require('lodash')
 
 const {
@@ -22,6 +23,19 @@ const RootQuery = new GraphQLObjectType({
             resolve(_, args) {
                 return axios
                         .get(`http://localhost:3000/users/${args.id}`)
+                        .then(res => res.data)
+            }
+        },
+        company: {
+            type: Company,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(_, args) {
+                return axios
+                        .get(`http://localhost:3000/companies/${args.id}`)
                         .then(res => res.data)
             }
         }
